8337f3562e466c10343196172a889c86e0176de3
Adriano Luzi <adriano.luzi16@gmail.com>
2026-07-01 17:55:52 -0300

    Agregue el apartado de soluciones y de hero, de esta forma el index esta sectorizado, también cambie la paleta de colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,9 +6,8 @@ export default {
     extend: {
       colors: {
         'kernel-dark': '#222121',
-        'kernel-olive': '#3F4D32',
-        'kernel-brown': '#5D4F35',
-        'kernel-gold': '#BF9E75',
+        'kernel-blue': '#4E6D93',
+        'kernel-green': '#C8DDA4',
         // El blanco (#FFFFFF) ya viene por defecto en Tailwind como 'white'
       }
     }
